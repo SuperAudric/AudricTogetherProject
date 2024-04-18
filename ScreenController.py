c532d23820bedf00ed2eb436b43eb584223ba6f0
Sigonith <166919350+Sigonith@users.noreply.github.com>
2024-04-18 13:22:23 +0100

    Update ScreenController.py
    Read through screen controller and added comments.

--- ScreenController.py
+++ ScreenController.py
@@ -1,28 +1,32 @@
 import pygame
 import os
 
-basepath = os.path.dirname(os.path.realpath(__file__)) #folder that this code was run in
-background = pygame.image.load(os.path.join(basepath,'Background.png'))
-gameWindow = pygame.display.set_mode((1200, 500))
+ownDirectory = os.path.dirname(os.path.realpath(__file__)) #folder that this code was run in
+background = pygame.image.load(os.path.join(ownDirectory,'Background.png')) # Variable type pygame image?
+gameWindow = pygame.display.set_mode((1200, 500))    # Another weird pygame variable; this one IS a window.
 
 
 def CreateWindow():
     pygame.init()
     pygame.display.set_caption("Ripoff Cave")
-    updateWindow()
+    updateWindow()            # This would normally be called every frame to update, but here is only updating once ever.
 
 
 def HasCloseWindowButtonBeenPressed():
     for event in pygame.event.get():
             if event.type == pygame.QUIT:
                  return True
-    return False;
+    return False
 
-def updateWindow():
-     gameWindow.blit(background,(0,0))
+# Ok, so all the drawing the map and the characters stuff needs to be here.
+def updateWindow(): 
+     gameWindow.blit(background,(0,0))  # 0,0 is top left corner of the screen.
      pygame.display.update()
 
 def CloseWindow():
     pygame.display.quit()
     pygame.quit()
     quit()
+
+
+
