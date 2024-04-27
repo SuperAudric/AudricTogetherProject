f73fedae9381eff004486c87d781f091b290903b
Sigonith <166919350+Sigonith@users.noreply.github.com>
2024-04-27 17:04:34 +0100

    Update CaveGameMain.py
    There is a bug somehow I'm unclear why

--- CaveGameMain.py
+++ CaveGameMain.py
@@ -5,33 +5,44 @@ import TestSecondFile as testName
 import ScreenController as ScreenController
 import pygame
 
-# Define some variables
-delay = 0.1
+# Define Globals
+delay = 0.1         # How often we update the screen.
+exitGame = False    # Whether we're still running the game.
+loopCount = 0   
 
-# Define the basic functions
+# Define Functions
 def setup():
-    testName.printHello();        # This is the tiny test file he built first.
-    print("I'm doing setup!")
-    ScreenController.CreateWindow()            # Wait oh we're initalizing the screen too.
+    print("Ok, time to start up.")
+    print("Let's initialize the window.")
+    ScreenController.CreateWindow()
 
-def mainLoop():
-    toPrint = "Main loop! Wee"
-    exit_game = False
-    while (exit_game == False):
-        print(toPrint)
+def scream():
+    screamString = "Main loop! Whee"
+    for x in range(loopCount):
+        screamString = screamString + "e"
+    print(screamString)
 
-        toPrint = toPrint +"e"
-        
-        time.sleep(delay)
-        
-        #check if someone pressed the X button
-        if(ScreenController.HasCloseWindowButtonBeenPressed()):
-            exit_game = True
-    ScreenController.CloseWindow()
-    
-#Run them
+
+
+
+# Run setup! Except for all the setup we just did.
 setup()
-mainLoop()            
+
+print("Starting main loop! The game is now running! Yay!")
+while (exitGame == False):
+
+    loopCount = loopCount + 1
+
+    # This is screaming "wheee" for some reason.
+    scream()
+
+    
+    time.sleep(delay)
+    
+    #check if someone pressed the X button
+    if(ScreenController.HasCloseWindowButtonBeenPressed()):
+        exitGame = True
+ScreenController.CloseWindow()      
 # May want to take the main loop out of a function, but put everything it's doing into functions instead so that it's very clearly chunked off with what does what.
 
 
